fa4ee34645a3cbed2781087c866524f7048238fc
Weronika Kuzio <weronikka004@gmail.com>
2021-12-07 17:47:47 +0100

    lab05

---
+++ lab05.tablice/lab05.tablice.cpp
@@ -0,0 +1,130 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	//char tab[] = "wiosna"; //lancuchowa
+	//char tab1[] = { 'n', 'e','a', 'd','\0' }; // inicjalizacja zbiorcza 
+
+	//char tab[100];
+	//cin >> tab;
+	//cin.getline(tab,50);
+	//cin.get(tab, 40);
+	/*
+	char tab[] = "ala";
+	//cout << tab;
+	cout << size(tab);
+	sizeof(tab) / sizeof(tab[10]);
+	//cin.get();
+	//cin.get(tab, 40).get;
+	//cout << tab;
+
+	char s2[]="ma kota";
+	strcpy_s(tab,s2);
+	//strcmp();
+	cout << strcat_s(tab, s2);
+
+	string imie;
+
+	imie.e
+	imie.swap //laczy dwa elementy
+	*/
+
+	/*ZADANIE1
+	char zdanie[] = { "lot" };
+	char zdanie[] = { 'l', 'o','t' };
+	*/
+	/*ZADANIE3
+	char tab[] = "wiosna";
+	cout << size(tab);
+	*/
+
+	//ZADANIE4
+	/*
+	string tekst1;
+	string tekst2;
+	string kopiowanie;
+	string laczenie;
+	cout << "wprowadz tekst1: ";
+	cin >> tekst1;
+	cout << "wprowadz tekst2: ";
+	cin >> tekst2;
+
+	laczenie = tekst1 + tekst2;
+	cout << "polaczony tekst1 i tekst 2: " << laczenie << endl;
+	cout << "dlugosc tekstu1: " << tekst1.size() << endl;;
+	kopiowanie.insert(0, tekst1);
+	cout << "tekst po skopiowaniu: " << kopiowanie << endl;
+	*/
+
+	//ZADANIE5
+	/*
+	char adres[100];
+	char numer[20];
+	int ocena;
+	int staz;
+
+
+	cout << "Podaj adres ulicy na ktorej mieszkasz: ";
+	cin.getline(adres, 100);
+
+	cout << "Podaj nr domu: ";
+	cin.getline(numer, 20);
+
+	cout << "Na ile oceniasz swoje umiejetnosci programowania w skali [2 - 5]:   ";
+	cin >> ocena;
+
+	cout << "Twoj staz progamistyczny: ";
+	cin >> staz;
+	cout << "Adres: " << adres << " " << numer << "\n";
+
+	cout << "Ocena: " << ocena - 1 << "\n";
+
+	cout << "Staz: " << staz << "dni";
+	*/
+
+	//ZADANIE 6
+	/*
+	char tekst[100];
+	int samogloski = 0, spolgloski = 0;
+
+	cout << "wprowadz tekst: ";
+	cin >> tekst;
+	for (int i = 0; tekst[i] != '\0'; ++i)
+	{
+		if (tekst[i] == 'a' || tekst[i] == 'e' || tekst[i] == 'i' || tekst[i] == 'o' || tekst[i] == 'u' || tekst[i] == 'y')
+		{
+			++samogloski;
+		}
+		else
+		{
+			++spolgloski;
+		}
+	}
+	cout << "liczba samoglosek: " << samogloski << endl;
+	cout << "liczba spolglosek: " << spolgloski;
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
